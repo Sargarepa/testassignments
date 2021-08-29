a7fee45418e944a039b06e7e88f47166185a602b
unknown <vorkapicmatija@gmail.com>
2021-08-29 16:51:52 +0200

    Add method that finds number of results returned from a google search to GoogleResultsPage

--- src/test/java/pageclasses/GoogleResultsPage.java
+++ src/test/java/pageclasses/GoogleResultsPage.java
@@ -1,13 +1,20 @@
 package pageclasses;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class GoogleResultsPage extends PageObject {
 	
+	private static final Logger log = LogManager.getLogger(GoogleResultsPage.class.getName());
+	
 	@FindBy(tagName = "h3")
 	private WebElement firstResult;
+	
+	@FindBy(id = "result-stats")
+	private WebElement resultStats;
 	
 	public GoogleResultsPage(WebDriver driver) {
 		super(driver);
@@ -23,4 +30,14 @@ public class GoogleResultsPage extends PageObject {
 		firstResult.click();
 		return new DemoQAHomePage(driver);
 	}
+	
+	public int numberOfSearchResults() {
+		String resultStatsText = resultStats.getText();
+		log.info("Result Stats Text = " + resultStatsText);
+		String numberOfResultsString = resultStatsText.split(" ")[1].replace(".", "");
+		int numberOfResults = Integer.parseInt(numberOfResultsString);
+		log.info("Number of search results = " + numberOfResults);
+		return numberOfResults;
+	}
+	
 }
