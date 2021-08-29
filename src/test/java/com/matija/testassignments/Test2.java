e0d62c724f00add33a0199179a3fb37c1c062b09
unknown <vorkapicmatija@gmail.com>
2021-08-29 16:52:44 +0200

    Add test for second assignment

---
+++ src/test/java/com/matija/testassignments/Test2.java
@@ -0,0 +1,27 @@
+package com.matija.testassignments;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pageclasses.GoogleHomePage;
+import pageclasses.GoogleResultsPage;
+
+public class Test2 extends FunctionalTest {
+
+	@Test
+	public void test2() {
+		driver.get("https://www.google.com/");
+		
+		GoogleHomePage googleHomePage = new GoogleHomePage(driver);
+		assertTrue(googleHomePage.isInitialized());
+		
+		GoogleResultsPage googleResultsPage = googleHomePage.searchAndSubmit("cheese");
+		assertTrue(googleResultsPage.isInitialized());
+		
+		int numberOfSearchResults = googleResultsPage.numberOfSearchResults();
+		
+		assertTrue("There is too much cheese on the internet", 777 > numberOfSearchResults);
+	}
+
+}
