4d42b538e3da313e94ea77fc6af6ff86c6cb4964
unknown <vorkapicmatija@gmail.com>
2021-08-30 06:08:57 +0200

    Add wait variable to Page Object superclass

--- src/test/java/pageclasses/PageObject.java
+++ src/test/java/pageclasses/PageObject.java
@@ -2,16 +2,18 @@ package pageclasses;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+//Abstract class to avoid making objects of it
 public abstract class PageObject {
 	
 	protected WebDriver driver;
+	protected WebDriverWait wait;
 	
 	public PageObject(WebDriver driver){
 		this.driver = driver;
+		this.wait = new WebDriverWait(driver, 5);
 		PageFactory.initElements(driver, this);
 	}
-	
-	public abstract boolean isInitialized();
-	
+
 }
