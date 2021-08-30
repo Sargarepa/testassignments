bd67f3357fae3e4820583a5c047c281ba78d19c7
unknown <vorkapicmatija@gmail.com>
2021-08-30 06:37:46 +0200

    Add Test Suite for running all three tests

---
+++ src/test/java/com/matija/testassignments/Test.java
@@ -0,0 +1,23 @@
+package com.matija.testassignments;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import pageclasses.DemoQAInteractionsPage;
+
+public class Test {
+
+	private static final Logger log = LogManager.getLogger(Test.class.getName());
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(AssignmentTestSuite.class);
+		for (Failure failure : result.getFailures()) {
+			log.info(failure.toString());
+		}
+		log.info(result.wasSuccessful());
+	}
+
+}
